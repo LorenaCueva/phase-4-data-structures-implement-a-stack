7b166678b064db213b3dc762565fba378a5e17b3
Lorena Cueva <lorenacueva@gmail.com>
2023-01-23 09:54:12 -0800

    commit tree a1febeba9fcc30a4201a42ef644ee353113a27cf

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,43 @@
-# your code here
+class Stack
+    attr_reader :stack, :limit
+
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        raise 'Stack Overflow' if full?
+        @stack << value
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+        @limit == @stack.size
+    end
+
+    def pop
+        @stack.pop
+    end 
+
+    def peek
+        @stack.last
+    end
+
+    def search(value)
+        @stack.each_with_index do |e, i|
+            if e == value 
+                return size - i - 1
+            end
+        end
+        -1
+    end
+
+end
